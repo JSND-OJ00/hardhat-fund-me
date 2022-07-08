aa8883adca38ed355d43dc727874b6870af39f29
JSND-OJ00 <jsnd.oj@gmail.com>
2022-07-08 11:50:14 +0900

    some changes

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -59,7 +59,23 @@ contract FundMe {
         s_funders.push(msg.sender);
     }
 
-    function Withdraw() public payable onlyOwner {
+    function withdraw() public payable onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        // Transfer vs call vs Send
+        // payable(msg.sender).transfer(address(this).balance);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    function cheaperWithdraw() public payable onlyOwner {
         address[] memory funders = s_funders;
         // mappings can't be in memory, sorry!
         for (
